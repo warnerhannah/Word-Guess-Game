6fbc4fd4217095823c09bc76ad95bb151f760b2a
Warnerhannah <warnerhannah1@yahoo.com>
2019-02-21 19:34:13 -0800

    rewrite

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,40 +4,93 @@ var winsTally = document.getElementById("wins");
 var wordDashes = document.getElementById("currentWordDashes");
 var remainingGuesses = document.getElementById("guessesRemaining");
 var lettersGuessed = document.getElementById("guessedLetters");
-var guesses = 10;
 
 var wordBank = ["apple", "pear"];
 
 
-// computer chooses which word to play 
+// Computer chooses which word to play 
 var correctWord = wordBank[Math.floor(Math.random() * wordBank.length)];
 console.log(correctWord);
 
-remainingGuesses.textContent = guesses;
-
+// Create Dashes 
 var dashesArray = [];
 for (var i = 0; i < correctWord.length; i++) {
     dashesArray[i] = "_";
 }
-wordDashes.textContent = dashesArray;
+wordDashes.textContent = dashesArray.join(" ");
 
-/* when user presses a key the game begins */
+//Letters Remaining
+var lettersRemaining = correctWord.length;
 
+// Wins Tally
+winsTally.textContent = 0;
+
+// Used Letters
+var usedLettersArray = [];
+
+//Amount of remaining guesses
+remainingGuesses.textContent = 10;
+guesses = remainingGuesses.textContent
+
+//when a key is pushed
 document.onkeyup = function (event) {
+    var userGuess = event.key;
+    if ((lettersRemaining > 0) && (guesses > 0)) {
+        playGame();
+    }
 
-    // takes in key as input and alerts user
+    else if (lettersRemaining === 0) {
+        winGame();
+    }
+
+    else {
+        alert("Sorry, you lose! The correct word was " + correctWord + ".")
+        resetGame();
+    }
+
+}
+
+// if they can continue to play
+function playGame() {
     var userGuess = event.key;
     alert("User guess: " + userGuess);
-
     
     if (correctWord.includes(userGuess)) {
         alert("You guessed correctly!")
-
+        dashesArray[i] = userGuess;
+        wordDashes.textContent = dashesArray.join(" ");
+        lettersRemaining--;
     }
 
-    //reduce remaining guesses
-    guesses--;
-    remainingGuesses.textContent=guesses;
+    else {
+        alert("You guessed incorrectly!");
+        usedLettersArray.push(userGuess);
+        lettersGuessed.textContent = usedLettersArray;
+    }
 
+    remainingGuesses.textContent--;
+    console.log(remainingGuesses);
+    console.log(guesses);
+
+    if (remainingGuesses === 0) {
+        endGame();
+    }
+
+    else if (lettersRemaining === 0) {
+        winGame();
+    }
+
+}
+
+// if they win the game & get the word
+function winGame() {
+    alert("Congratulations! You Won! The correct word was " + correctWord + ".");
+    winsTally.textContent++;
+    resetGame()
+}
+
+//reset the game
+function resetGame() {
+    guesses = 10;
 
 }
